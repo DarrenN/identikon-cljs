47724b821ad2b17a2c6b933951ea4f7811a34af4
DarrenN <info@v25media.com>
2016-02-21 11:04:45 -0500

    Use thi.ng color and svg.js to render identikons

--- src/identikon_cljs/core.cljs
+++ src/identikon_cljs/core.cljs
@@ -1,24 +1,14 @@
 (ns identikon-cljs.core
   (:require
    [cljs-hash.goog :as gh]
-   [cljsjs.snapsvg]
+   [thi.ng.color.core :as col]
    [cljsjs.svgjs]))
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over-written on reload
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  )
-
 (defn log-str
   ([x]   (do (.log js/console (pr-str x)) x))
   ([m x] (do (log-str {:msg m :data x})   x)))
-
-(log-str "hi" js/SVG)
 
 (defn sha1 [str]
   (gh/sha1-hex str))
@@ -31,22 +21,23 @@
 
 (defn create-mirror [l]
   "Create a list of ints, break into chunks of 5 and convert those into mirrored chunks so that [0 1 2 3 4] becomes [0 1 2 1 0] and then flatten it all back out"
-  (let [h (take 15 l)
-        t (reverse (take 10 l))
-        f (partition 5 (flatten (cons h t)))
+  (let [t (reverse (take 5 l))
+        f (partition 5 (flatten (cons l t)))
         m (map #(flatten (cons (take 3 %) (reverse (take 2 %)))) f)]
     (flatten m)))
 
 (defn convert-string [s]
+  "Turn the hash string into a collections of integers (0-255)"
   (-> s
       sha1
       to-pairs
       to-ints))
 
 (defn get-svg [id]
+  "SVG.js converts an SVG to 100% x 100% on grabbing the DOM element so we force it back to its original dimensions and pass them back with the element"
   (let [d (.getElementById js/document id)
-        w (.getAttribute d "width")
-        h (.getAttribute d "height")
+        w (js/parseInt (.getAttribute d "width") 10)
+        h (js/parseInt (.getAttribute d "height") 10)
         p (js/SVG id)]
     (.spof p)
     (.attr p "width" w)
@@ -63,13 +54,15 @@
   (sort (mapv #(Math/floor (* 1.411 %)) l))) ;; convert 0-255 to 0-360
 
 (defn find-hues [ints]
-  (let [remains (drop 15 ints)]
-    (vector (first remains) (last remains))))
+  "Add up the first 3 and last 3 integers of the last 5 to generate a start and end range for the hues (0-255)"
+  (let [remains (drop 15 ints)
+        h (take 3 remains)
+        t (drop 2 remains)]
+    (vector (mod (apply + h) 255) (mod (apply + t) 255))))
 
 (defn get-svg-attrs [p]
-  "Extract a bunch of attributes from the SVG object and calculate the x,y positions
-for all the circles and their radii"
-  (let [{svg :svg rwidth :w rheight :h} p
+  "Use the width and height of the SVG object to calculate the x,y positions for all the circles and their radii"
+  (let [{svg :svg rwidth :width rheight :height} p
         attr (.attr svg)
         amt 5
         total (* amt amt)
@@ -78,7 +71,7 @@ for all the circles and their radii"
         xoffset (/ (* rwidth .1) 2) ; amount to offset the dots on x/y
         roffset (/ (* width .1) 12)
         cwidth (Math/floor (/ width amt)) ; circle width
-        crad (- (/ cwidth 2) roffset) ; circle radius
+        crad (- (* cwidth .85) roffset) ; circle radius
         cxs (mapv #(* cwidth %)
                   (drop 1 (take (+ 1 amt) (range)))) ; vector of x coords
         ]
@@ -94,33 +87,39 @@ for all the circles and their radii"
                   [y x])
      }))
 
+(defn make-hsla [hue]
+  "Create a string hex color from a hue, ex: #ff0000"
+  (let [hsla (clojure.string/replace "hsla(_,80%,50%,1)" #"_" (str hue))]
+    @(-> hsla col/css col/as-int24 col/as-css)))
+
 (defn make-circle-zero [p triple xoff roff cr]
   "Large solid circle"
   (let [[x y hue] triple
         c (.circle p (- cr roff))
-        hue (js/Snap.hsl hue 80 50)]
+        hsla (make-hsla hue)]
     (.attr c (clj->js {:cx (- (+ xoff x) cr)
                        :cy (- (+ xoff y) cr)
-                       :fill hue}))))
+                       :fill hsla}))))
 
 (defn make-circle-one [p triple xoff roff cr]
   "Ring"
   (let [[x y hue] triple
         c (.circle p (- (- cr (/ cr 4)) roff))
-        hue (js/Snap.hsl hue 80 50)]
-    (log-str #js [p c])
+        hsla (make-hsla hue)]
     (.attr c (clj->js {:cx (- (+ xoff x) cr)
                        :cy (- (+ xoff y) cr)
-                       :fill hue}))))
+                       :fill "#fff"
+                       :stroke-width (/ cr 4)
+                       :stroke hsla}))))
 
 (defn make-circle-two [p triple xoff roff cr]
   "Small solid circle"
   (let [[x y hue] triple
         c (.circle p (- (- cr (/ cr 3)) roff))
-        hue (js/Snap.hsl hue 80 50)]
+        hsla (make-hsla hue)]
     (.attr c (clj->js {:cx (- (+ xoff x) cr)
                        :cy (- (+ xoff y) cr)
-                       :fill hue}))))
+                       :fill hsla}))))
 
 (defn make-identikon [p ints hues]
   "Determine which circle to show by converting the int to mod 3"
